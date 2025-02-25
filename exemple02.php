9847e03ef03a4fb02bc206ae7a7e669a8b0bf192
Pier-Luc Brault <pier-luc@brault.me>
2025-02-24 19:29:40 -0500

    Ajout des exemples

---
+++ exemple02.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exemple 02</title>
+    <link rel="stylesheet" href="water.css">
+  </head>
+  <body>
+    <nav><a href="index.php">Retour</a></nav>
+    <h1>Bonjour</h1>
+    <?php
+      echo 'Ce texte est généré par PHP.';
+      echo '<br /><strong>Je peux aussi utiliser des balises HTML.</strong>';
+    ?>
+  </body>
+</html>
---
+++ exemple13.php
@@ -0,0 +1,28 @@
+<?php
+  $noExemple = 13;
+
+  $prenom = 'Homer';
+  $nom = 'Simpson';
+  $anneeNaissance = 1956;
+
+  // Concaténation avec les apostrophes incluant un int
+  $presentation = 'Bonjour, mon nom est <strong>'
+    . $prenom. ' ' . $nom . '</strong> '
+    . 'et je suis né en ' . $anneeNaissance . '.';
+    ;
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Exemple <?php echo $noExemple; ?>
+    </title>
+    <link rel="stylesheet" href="water.css">
+  </head>
+  <body>
+    <nav><a href="index.php">Retour</a></nav>
+    <h1>Exemple <?php echo $noExemple; ?></h1>
+    <?php echo $presentation; ?>
+  </body>
+</html>
